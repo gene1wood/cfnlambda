1533100ad834df9e7e15181fbffa4d314a76d2fa
sellers <chris.sellers@ithaka.org>
2016-10-21 21:14:10 -0400

    fixed logging declaration
    fixed a bug with the logging declaration that was causing lambda functions to
    fail
    added some pep257 comments to the methods

--- examples/example.py
+++ examples/example.py
@@ -1,13 +1,15 @@
+"""Example lambda function code for cfn."""
 from cfnlambda import handler_decorator
 import logging
 
-logger = logging.getLogger(__name__)
-logging.getLogger().setLevel(logging.INFO)
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
 
 
 @handler_decorator()
 def lambda_handler(event, context):
+    """lambda handler method entry point."""
     result = (float(event['ResourceProperties']['key1']) +
               float(event['ResourceProperties']['key2']))
-    logger.info("Here is an example log message")
+    LOGGER.info("Here is an example log message")
     return {'sum': result}
